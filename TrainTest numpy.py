74dee375163d0afd448c3af2364474428ab2eab6
varada2702 <63180848+varada2702@users.noreply.github.com>
2020-12-11 23:39:26 +0530

    Update TrainTest numpy.py

--- TrainTest numpy.py
+++ TrainTest numpy.py
@@ -73,9 +73,68 @@ y_test = np.array(test_pd['Momentum'], dtype = 'float')
 
 
 #Using SVM 
-
-
-model = SVC()
-model.fit(X_train, y_train)
-ypred = model.predict(X_test)
+model1 = SVC()
+model1.fit(X_train, y_train)
+ypred = model1.predict(X_test)
 accuracy_score(y_test, ypred)
+
+
+#Using Ridge Regression
+def error_func(w, Xtest, Ytest):
+    yhat = Xtest@w
+    error = []
+    for i in range(len(Ytest)):
+        error.append(np.subtract(Ytest, yhat))
+        
+    return sum(error)/len(error)
+
+
+#Ridge function takes the training and test datasets and the D/I matrix
+#For a range of lambdas it checks which give the least error and returns 
+#the weights that give the minimum error
+    
+def ridgeX(x_train, y_train, x_test, y_test, matrix): 
+       
+    min_w = np.linalg.inv(x_train.T@x_train + 1*matrix)@x_train.T@y_train
+    #Here the min_w is calculated with lambda = 1
+
+    min_error = error_func(min_w, x_test, y_test)    
+    #print(min_error.shape)
+
+    for i in range(2,10):
+        w = np.linalg.inv(x_train.T@x_train + i*matrix)@x_train.T@y_train
+        #print(w.shape)
+
+        error = error_func(w, x_test, y_test)
+        #print(error.shape)
+
+        if((sum(error)/len(error))<(sum(min_error)/len(error))):
+            min_w = w
+            min_error = error
+        #This checks if the errors of each lambda are lesser than the errors when lambda = 1. If yes then it gives 
+        # weights corresponding to that lambda    
+
+    return min_w
+
+
+# Making D Matrix
+D = np.zeros((6,6))
+
+D[0,0] = 1
+D[1,0] = -2
+D[1,1] = 1
+
+for i in range(2,6):
+    D[i,i] = 1
+    D[i, i-1] = -2
+    D[i, i-2] = -1
+    
+    
+# Making Identity matrix    
+I = n,m = X_train.shape
+I = np.identity(m)    
+    
+
+
+w_ridgeD = ridgeX(X_train, y_train, X_test, y_test, D)
+w_ridgeI = ridgeX(X_train, y_train, X_test, y_test, I)
